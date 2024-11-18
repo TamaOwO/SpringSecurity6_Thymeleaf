e5706d799b0d5a5f0f0e5cbc300ff25aa670b4da
Tama <mytamhuynhhmt@gmail.com>
2024-11-18 18:48:06 +0700

    fix bug

--- src/main/java/com/example/springsecurity6_thymeleaf/repository/RoleRepository.java
+++ src/main/java/com/example/springsecurity6_thymeleaf/repository/RoleRepository.java
@@ -1,6 +1,7 @@
 package com.example.springsecurity6_thymeleaf.repository;
 
 import com.example.springsecurity6_thymeleaf.entiry.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -8,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface RoleRepository {
+public interface RoleRepository extends JpaRepository<Role, Long> {
     @Query("SELECT u FROM Role u WHERE u.name = :name")
     public Role getUserByName(@Param("name") String name);
     Optional<Role> findByName(String name);
